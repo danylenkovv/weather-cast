7b945adca33faad95f696e4449f9ee22c7812c2e
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-04 10:33:23 +0200

    App.php - Create render func and default action

---
+++ app/core/App.php
@@ -0,0 +1,29 @@
+<?php
+
+class App
+{
+
+    /**
+     * Renders the specified view.
+     *
+     * @param string $page The name of the view to render.
+     * @param array $data Data to pass to the view.
+     * @param string $layout The name of the layouts to use.
+     * @return void
+     */
+    public function render(string $page, array $data = [], string $layout = 'default'): void
+    {
+        extract($data);
+        require "app/views/layouts/{$layout}.php";
+    }
+
+    /**
+     * Index action that checks if the user is authenticated and retrieves notes if they are.
+     *
+     * @return void
+     */
+    public function index(): void
+    {
+        $this->render('daily');
+    }
+}
