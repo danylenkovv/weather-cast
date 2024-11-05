09e675af04344202481beb83019ec69ea1e14454
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-05 20:21:50 +0200

    WeatherController.php - Create control function for weekly forecast

--- app/controllers/WeatherController.php
+++ app/controllers/WeatherController.php
@@ -19,4 +19,23 @@ class WeatherController
             'hours' => $hourlyWeather
         ]);
     }
+
+    /**
+     * Displays the current weather and daily forecast for the specified city and days.
+     *
+     * @param string $city The name of the city for which to display weather data.
+     * @param int $days The number of days for forecast - 7 or 14.
+     * @return void
+     */
+    public function weekly(string $city, int $days = 7): void
+    {
+        $model = new Forecast();
+        $currentWeather = $model->getCurrent($city);
+        $weeklyWeather = $model->getWeekly($city, $days);
+
+        App::render('weekly', [
+            'current' => $currentWeather,
+            'weekly' => $weeklyWeather
+        ]);
+    }
 }
