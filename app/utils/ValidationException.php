b54e436afca4893d75caabc964cd8ba9ef6f9902
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-17 19:11:38 +0200

    ValidationException.php - Create exception class for validation

---
+++ app/utils/ValidationException.php
@@ -0,0 +1,21 @@
+<?php
+
+class ValidationException extends Exception
+{
+    private $description;
+
+    public function __construct($message = "No errors", $code = 0, Exception $previous = null, $description = '')
+    {
+        $this->description = $description;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * Returns error's description
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+}
