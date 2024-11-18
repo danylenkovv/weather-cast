ae6ea2ec20902ceb9d27c8e8a2cfb31b6053773d
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-18 14:07:12 +0200

    IpLookup.php - Create model to retrieve client location by ip

---
+++ app/models/IpLookup.php
@@ -0,0 +1,17 @@
+<?php
+class IpLookup extends Model
+{
+    private string $endpoint = 'ip.json';
+
+    /**
+     * Retrieves location data from API by IP address and returns city
+     * @param string $ip
+     * @return string
+     */
+    public function getLocationByIp(string $ip): string
+    {
+        $url = $this->getApiUrl($this->endpoint, $ip);
+        $data = $this->fetchData($url);
+        return $data['city'];
+    }
+}
