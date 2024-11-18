c86bea583a75b1144b7b45e5aefa2ae255f3b2b8
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-18 14:05:29 +0200

    Helpers.php - Create getIp() to retrieve real client ip address

--- app/utils/Helpers.php
+++ app/utils/Helpers.php
@@ -121,4 +121,20 @@ class Helpers
 
         return 'current';
     }
+    /**
+     * Get real client IP
+     * @return string
+     */
+    public static function getIp(): string
+    {
+        $ip = '';
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
 }
