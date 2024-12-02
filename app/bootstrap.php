7c5ca54552124f41872d059ce23f18aa73dc1f3c
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-12-02 17:05:30 +0200

    bootstrap.php - Add functions for config loading

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,38 @@
 <?php
 
 require_once 'config.php';
-require_once '../key.php';
+/**
+ * Load params from .conf file
+ * @throws \app\utils\SystemException
+ * @return void
+ */
+function loadConfig(): void
+{
+    $configFile = '..' . DIRECTORY_SEPARATOR . '.conf';
+    if (!file_exists($configFile)) {
+        throw new app\utils\SystemException('Internal Server Error', 500, null, 'Service currently unavailable, try again later');
+    }
+    $handle = fopen($configFile, 'r');
+    if (!$handle) {
+        throw new app\utils\SystemException('Internal Server Error', 500, null, 'Service currently unavailable, try again later');
+    }
+    while (!feof($handle)) {
+        $row = trim(fgets($handle));
+        if (!empty($row) && strpos($row, '#') !== 0) {
+            putenv($row);
+        }
+    }
+}
+
+/**
+ * Return value of conf param
+ * @param string $key
+ * @return array|bool|string
+ */
+function conf(string $key)
+{
+    return getenv($key);
+}
 
 spl_autoload_register(function ($class) {
     $path = '..' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
@@ -11,6 +42,10 @@ spl_autoload_register(function ($class) {
     }
     return false;
 });
+
+loadConfig();
+define('BASE_URL', conf('BASE_URL'));
+define('API_KEY', conf('API_KEY'));
 
 $router = new \app\core\Router();
 $router->init();
