5daaee007f193d7c8313f23fa9d3accc7a7bd08f
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-12-02 17:06:17 +0200

    SystemException.php - Create exception class for another system reasons

---
+++ app/utils/SystemException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\utils;
+
+use Exception;
+
+class SystemException extends Exception
+{
+    private $description;
+
+    public function __construct($message = "No errors", $code = 0, Exception $previous = null, $description = '')
+    {
+        $this->description = $description;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * Returns error's description
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+}
