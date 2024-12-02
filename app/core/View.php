10ee5a9ded61e56c962f86dfe248617fa4217452
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-12-02 16:24:13 +0200

    View.php - Refactore code

--- app/core/View.php
+++ app/core/View.php
@@ -4,19 +4,28 @@ namespace app\core;
 
 class View
 {
+    const VIEWS_DIR = '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
+
+    public static $layout = 'default';
+
+    public function __construct(string $layout = null)
+    {
+        if ($layout) {
+            $this->layout = $layout;
+        }
+    }
 
     /**
      * Renders the specified view.
      *
      * @param string $page The name of the view to render.
      * @param array $data Data to pass to the view.
-     * @param string $layout The name of the layouts to use.
      * @return void
      */
-    public static function render(string $page, array $data = [], string $layout = 'default'): void
+    public static function render(string $page, array $data = []): void
     {
         extract($data);
-        require "../app/views/layouts/{$layout}.php";
+        include_once self::VIEWS_DIR . 'layouts' . DIRECTORY_SEPARATOR . self::$layout . '.php';
         exit();
     }
 }
