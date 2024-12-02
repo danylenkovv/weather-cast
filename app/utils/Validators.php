767c27df2b7a0ba63899924d1762a8596a9f5f6e
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-12-02 16:21:54 +0200

    Validators.php - Remove unused methods

--- app/utils/Validators.php
+++ app/utils/Validators.php
@@ -21,19 +21,6 @@ class Validators
     }
 
     /**
-     * Validates the forecast period.
-     *
-     * @param int $days The number of days to validate.
-     * @throws ValidationException If the forecast period is not in the allowed range.
-     */
-    public static function validateDays(int $days): void
-    {
-        if (!in_array($days, FORECAST_DAYS)) {
-            throw new ValidationException("Bad Request", 400, null, 'Incorrect forecast period. Must be 7 or 14 days');
-        }
-    }
-
-    /**
      * Validates the date input format and range.
      *
      * @param string $date The date to validate.
@@ -49,20 +36,6 @@ class Validators
         }
         if ($date < $startDate || $date > $endDate) {
             throw new ValidationException("Bad Request", 400, null, "Incorrect date input. Date must be in range: 1 year ago - 300 days ahead");
-        }
-    }
-
-    /**
-     * Validates the inputed URL params
-     * @throws ValidationException If the date is invalid or out of the allowed range.
-     */
-    public static function validateUrlParams(): void
-    {
-        $queryParams = array_keys($_GET);
-
-        $invalidParams = array_diff($queryParams, ALLOWED_URL_PARAMS);
-        if (!empty($invalidParams)) {
-            throw new ValidationException("Bad Request", 400, null, 'Incorrect URL parameters passed. Allowed parameters: action, date');
         }
     }
 }
