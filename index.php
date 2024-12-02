829d0c9cb398193fc8d4b2ade985c3dcde599b5c
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-12-02 12:32:23 +0200

    Move index.php to public directory

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try {
-    require_once 'app/config/bootstrap.php';
-} catch (ValidationException $e) {
-    Helpers::handleException($e);
-} catch (\Exception $e) {
-    Helpers::handleException($e);
-}
